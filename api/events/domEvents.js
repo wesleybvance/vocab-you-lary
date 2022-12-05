bfdeef5b004ff8028e95e2daef7ebcc70aed8459
Wesley Vance <wslyvnce@gmail.com>
2022-12-04 22:48:56 -0600

    built and tested delete word functionality

--- api/events/domEvents.js
+++ api/events/domEvents.js
@@ -1,22 +1,20 @@
-// import addWordForm from '../../components/forms/addWordForm';
-// // import renderToDOM from '../../utils/renderToDom';
+import showWords from '../pages/words';
+import { deleteWord, getWords } from '../wordsData';
+// import renderToDOM from '../../utils/renderToDom';
 
-// const domEvents = (user) => {
-//   document.querySelector('#app').addEventListener('click', (e) => {
-//     // CLICK DELETE WORD
-//   //   if (e.target.id.includes('delete-word')) {
-//   //     if (window.confirm('Are you sure you want to delete this entry?')) {
-//   //       const [, firebaseKey] = e.target.id.split('--');
-//   //       // add delete function from wordData
-//   //     }
-//   //   }
-//   //   if (e.target.id.includes('add-vocab')) { //   // add word form function
-//   // }
-//     if (e.target.id.includes('add-word-link')) {
-//       console.warn('clicked');
-//       addWordForm(user.uid);
-//     }
-//   });
-// };
+const domEvents = (user) => {
+  document.querySelector('#app').addEventListener('click', (e) => {
+    // CLICK DELETE WORD
+    if (e.target.id.includes('delete-word')) {
+      // eslint-disable-next-line no-alert
+      if (window.confirm('Are you sure you want to delete this entry?')) {
+        const [, firebaseKey] = e.target.id.split('--');
+        deleteWord(firebaseKey).then(() => {
+          getWords(user.uid).then(showWords);
+        });
+      }
+    }
+  });
+};
 
-// export default domEvents;
+export default domEvents;
